0b1deaf6e3f1fba26a04e98810c7675eeabcaa73
ADRathod1614 <125894378+ADRathod1614@users.noreply.github.com>
2023-04-10 17:51:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const path=require('path')
+const express=require('express');
+const nav=express();
+const port=3000;
+//built in middleware
+//relative absolute
+//console.log(__dirname);
+
+const staticPath=path.join(__dirname,'../static website');
+nav.use(express.static(staticPath));
+
+nav.get('/',(req,res)=>{
+    res.send("welcome to my page")
+});
+nav.get('/aboutus',(req,res)=>{
+    res.send("welcome to my about page")
+});
+nav.get('/temp',(req,res)=>{
+    res.status(404).send("welcome to my temp page")
+});
+nav.listen(port,()=>{
+    console.log(`listening to port ${port}`)
+})
